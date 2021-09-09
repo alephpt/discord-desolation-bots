191c989aa9714c8b13a7467db7fe2621f45fff5c
nopeless <38830903+nopeless@users.noreply.github.com>
2021-09-09 18:48:02 +0900

    add an eslintrc file for linting

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "parserOptions": {
+    "sourceType": `module`,
+    "ecmaVersion": 2020
+  },
+  // ignore certain files
+  "ignorePatterns": [`docs/**/*.js`, `?.js`],
+  "extends": `eslint:recommended`,
+  "rules": {
+    "quotes": [1, `backtick`],
+
+    "indent": [`error`, 2],
+    "no-trailing-spaces": [`error`, { "skipBlankLines": false }],
+    "semi": [`error`, `always`],
+
+    "linebreak-style": [`error`, `unix`],
+  }
+};
